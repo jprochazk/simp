6e767dd3bd074f9bb6ef8897c238e9ebc2ef1809
jprochazk <1665677+jprochazk@users.noreply.github.com>
2025-08-17 19:50:01 +0200

    actually implement tests

--- src/parser/tests.rs
+++ src/parser/tests.rs
@@ -1,1 +1,12 @@
+fn parse(code: &str) -> String {
+    match super::parse(code) {
+        Ok(ast) => format!("{ast}"),
+        Err(err) => format!("{}", err.render(code)),
+    }
+}
 
+#[glob_test::glob("./inputs/**/*.simp")]
+fn test_parse(path: &std::path::Path) {
+    let input = std::fs::read_to_string(path).unwrap();
+    insta::assert_snapshot!(parse(&input));
+}
